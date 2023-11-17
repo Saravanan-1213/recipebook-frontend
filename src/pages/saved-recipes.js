69734c49c9acd4281fbd27f07d717f9864e38c8e
Saravanan-1213mm <saravanansekar033@gmail.com>
2023-11-18 00:03:48 +0530

    Recipebook-Frontend

---
+++ src/pages/saved-recipes.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useGetUserID } from "../hooks/useGetUserID";
+
+const SavedRecipes = () => {
+  const [savedRecipes, setSavedRecipes] = useState([]);
+
+  const userID = useGetUserID();
+  useEffect(() => {
+    const fetchSavedRecipe = async () => {
+      try {
+        const response = await axios.get(
+          `https://recipebook-backend.onrender.com/recipes/savedRecipes/${userID}`
+        );
+        setSavedRecipes(response.data.savedRecipes);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchSavedRecipe();
+  }, []);
+
+  return (
+    <div className="saved">
+      <h1>Saved Recipes</h1>
+      <ul>
+        {savedRecipes.map((recipe) => {
+          return (
+            <li key={recipe._id}>
+              <div>
+                <h2>{recipe.name}</h2>
+              </div>
+              <div className="instructions">
+                <p>{recipe.instructions}</p>
+              </div>
+              <img src={recipe.imageUrl} alt={recipe.name} />
+              <p>Cooking Time: {recipe.cookingTime} (minutes)</p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default SavedRecipes;
